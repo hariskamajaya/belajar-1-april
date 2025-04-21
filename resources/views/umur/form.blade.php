6201b0921110083d6b2e2f0e50332d3e8d056f4d
fahmifn.idn@gmail.com <fahmifn.idn@gmail>
2025-04-21 16:10:06 +0700

    1.5

---
+++ resources/views/umur/form.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Form</title>
+</head>
+<body>
+    <h1>Halaman Form Umur</h1>
+    <p>Masukan data kamu dibawah ini : </p>
+
+    <form action="#" method="post">
+        <div class="">
+            <label for="">Nama Lengkap</label>
+            <input type="text" name="nama" required placeholder="masukan nama kamu">
+        </div>
+        <div class="">
+            <label for="">Umur</label>
+            <input type="number" name="umur" required placeholder="masukan umur kamu">
+        </div>
+        <div class="">
+            <button type="submit">Masuk</button>
+        </div>
+    </form>
+
+
+</body>
+</html>
